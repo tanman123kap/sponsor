f06e29bae7cf534ebc04b0b3c1e700e2c763b357
tanman123kap <tanman123kap@gmail.com>
2025-03-10 14:20:27 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -33,9 +33,9 @@ app.get("/show.html", async (req, res) => {
 app.post("/form", async (req, res) => {
     try {
         const { sponsor_id, id, name } = req.body;
-        const user_id = await sponsorModel.findOne({ sponsor_id });
+        const user_id = await sponsorModel.exists({ sponsor_id });
         console.log(user_id);
-        if(user_id) {
+        if(user_id === true) {
             const formData = {
                 sponsor_id: Number(sponsor_id),
                 id: Number(id),
